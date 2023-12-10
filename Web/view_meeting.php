8d2a50785db315c88e41b35c0633f439b6659324
Nguyễn Hoàng Tín <nguyenhoangtin1404@gmail.com>
2023-12-10 21:37:30 +0700

    Update view_meeting.php

--- Web/view_meeting.php
+++ Web/view_meeting.php
@@ -29,6 +29,7 @@ if ($meetingId) {
         $speaker = $meeting['speaker'];
         $speechType = $meeting['speech_type'];
         $countdownTime = $meeting['countdown_time'];
+        $isUpdate = $meeting['is_update'];
     } else {
         echo "Không tìm thấy kỳ họp.";
         exit();
@@ -47,6 +48,12 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thông tin kỳ họp</title>
+    <style>
+        #countdown {
+            font-size: 20px;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
     <h1>Thông tin kỳ họp</h1>
@@ -54,32 +61,58 @@ $conn->close();
     <p><strong>Nội dung:</strong> <?php echo $content; ?></p>
     <p><strong>Tên người phát biểu:</strong> <?php echo $speaker; ?></p>
     <p><strong>Loại phát biểu:</strong> <?php echo $speechType; ?></p>
-    <p><strong>Thời gian đếm ngược:</strong> <?php echo $countdownTime; ?></p>
+    <p><strong>Thời gian đếm ngược:</strong> <span id="countdown"></span></p>
 
+    <script>
+        // Lấy thời gian đếm ngược và trạng thái is_update từ PHP
+        var countdownTime = <?php echo $countdownTime; ?>;
+        var isUpdate = <?php echo $isUpdate; ?>;
 
-<!-- Đoạn JavaScript thêm vào trang view_meeting.php -->
-<script>
-    function updateIsUpdate() {
-        // Sử dụng XMLHttpRequest để gửi yêu cầu đến trang xử lý PHP
-        var xhttp = new XMLHttpRequest();
+        // Hiển thị thời gian đếm ngược
+        function displayCountdown() {
+            var minutes = Math.floor(countdownTime / 60);
+            var seconds = countdownTime % 60;
 
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                // Nhận phản hồi từ trang xử lý PHP (nếu cần)
-                console.log(this.responseText);
+            // Định dạng hiển thị giờ, phút, giây
+            var formattedTime = minutes + " phút " + seconds + " giây";
+
+            // Hiển thị lên trang web
+            document.getElementById("countdown").innerHTML = formattedTime;
+
+            // Giảm thời gian đếm ngược
+            countdownTime--;
+
+            // Kiểm tra xem đã hết thời gian đếm ngược chưa
+            if (countdownTime < 0) {
+                clearInterval(countdownInterval);
+                document.getElementById("countdown").innerHTML = "Hết thời gian";
             }
-        };
+        }
 
-        // Gửi yêu cầu đến trang xử lý PHP để kiểm tra và cập nhật is_update
-        xhttp.open("GET", "update_is_update.php?id=<?php echo $meetingId; ?>", true);
-        xhttp.send();
-    }
+        // Gọi hàm hiển thị đếm ngược mỗi giây
+        var countdownInterval = setInterval(displayCountdown, 1000);
 
-    // Lặp lại hàm kiểm tra và cập nhật mỗi giây
-    setInterval(updateIsUpdate, 1000);
-</script>
+        // Kiểm tra và cập nhật is_update mỗi giây
+        function updateIsUpdate() {
+            // Nếu is_update = 0, thực hiện cập nhật
+            if (isUpdate === 0) {
+                var xhttp = new XMLHttpRequest();
+
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        // Nhận phản hồi từ trang xử lý PHP (nếu cần)
+                        console.log(this.responseText);
+                    }
+                };
+
+                // Gửi yêu cầu đến trang xử lý PHP để kiểm tra và cập nhật is_update
+                xhttp.open("GET", "update_is_update.php?id=<?php echo $meetingId; ?>", true);
+                xhttp.send();
+            }
+        }
+
+        // Lặp lại hàm kiểm tra và cập nhật mỗi giây
+        setInterval(updateIsUpdate, 1000);
+    </script>
 </body>
 </html>
-
-
-
